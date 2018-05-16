0c4bd16fedede44b7c93cff128624bcc896bbbf8
Charles McQuain <charlesmcquain@gmail.com>
2018-05-15 22:22:43 -0700

    Added Event listener for Enter Keypress in Note input

--- app/main.js
+++ app/main.js
@@ -1,10 +1,19 @@
 const inputComponent = {
-  template: `<input :placeholder="placeholder" v-model="input" class="input is-small" type="text" />`,
+  template: `<input :placeholder="placeholder" v-model="input" @keyup.enter="monitorEnterKey" class="input is-small" type="text" />`,
   props: ["placeholder"],
   data() {
     return {
       input: ""
     };
+  },
+  methods: {
+    monitorEnterKey() {
+      this.$emit("add-note", {
+        note: this.input,
+        timestamp: new Date().toLocaleString()
+      });
+      this.input = "";
+    }
   }
 };
 
@@ -17,5 +26,11 @@ new Vue({
     notes: [],
     timestamples: [],
     placeholder: "Enter a note"
+  },
+  methods: {
+    addNote(event) {
+      this.notes.push(event.note);
+      this.timestamps.push(event.timestamp);
+    }
   }
 });
